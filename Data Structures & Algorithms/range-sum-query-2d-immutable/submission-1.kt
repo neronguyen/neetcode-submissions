6b2736b3c93ff91a3c0ad2c26b04fd135a445399
Nero Nguyen <50225603+neronguyen@users.noreply.github.com>
2026-08-02 17:52:03 +0700

    Add: range-sum-query-2d-immutable - submission-1

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-1.kt
@@ -0,0 +1,23 @@
+class NumMatrix(private val matrix: Array<IntArray>) {
+
+    private val rows get() =  matrix.size
+    private val prefixSums = Array(rows) { row -> 
+        matrix[row].scan(0) { cur, next -> cur + next }
+    }
+
+    fun sumRegion(row1: Int, col1: Int, row2: Int, col2: Int): Int {
+        var sum = 0
+        for (i in row1..row2) {
+            val width = prefixSums[i][col2 + 1] - prefixSums[i][col1]
+            sum += width
+        }
+
+        return sum
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = NumMatrix(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
