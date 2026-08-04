7eb8f2171b86c4f0331f6f546afbd993842c2e55
Nero Nguyen <50225603+neronguyen@users.noreply.github.com>
2026-08-04 08:35:07 +0700

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        val n = nums.size
+        val prefixSums =  nums.scan(1, Int::times)
+        val reversedPrefixSums = IntArray(n + 1) { nums.getOrNull(it) ?: 1 }
+        for (i in nums.indices.reversed()) {
+            reversedPrefixSums[i] = reversedPrefixSums[i] * reversedPrefixSums[i + 1]
+        }
+
+        println(prefixSums)
+        println(reversedPrefixSums.joinToString(", "))
+    
+        val result = IntArray(nums.size) {
+            prefixSums[it] * reversedPrefixSums[it + 1]
+        }
+
+        return result
+    }
+}
